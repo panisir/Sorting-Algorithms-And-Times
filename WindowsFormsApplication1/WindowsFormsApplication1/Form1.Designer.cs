3f82785e7bbbd04a341f8de14b8cf7b24967b03e
panisir <melihh.korkmaz@gmail.com>
2016-03-23 20:19:41 +0200

    Sorting Algorithms And Sorting Times
    This Windows Form app has capabilities like Generating Random Numbers
    And Sort them using Merge, Selection and Quick Sort algorithms. The app
    shows the difference of sorting times between these three algorithms.

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SayiUret = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.numeric1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(164, 381);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(425, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(164, 381);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // SayiUret
+            // 
+            this.SayiUret.Location = new System.Drawing.Point(238, 12);
+            this.SayiUret.Name = "SayiUret";
+            this.SayiUret.Size = new System.Drawing.Size(131, 35);
+            this.SayiUret.TabIndex = 2;
+            this.SayiUret.Text = "Generate Random Numbers";
+            this.SayiUret.UseVisualStyleBackColor = true;
+            this.SayiUret.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(238, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Selection Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(238, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Quick Sort";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(238, 135);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 35);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Merge Sort";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(229, 358);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 35);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Print Sorted Array";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Quick Sorting Time:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Random Üretme Zamanı:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(314, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(314, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(182, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Selection Sorting Time:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(314, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(182, 257);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Merge Sorting Time:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(314, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "0";
+            // 
+            // numeric1
+            // 
+            this.numeric1.Location = new System.Drawing.Point(229, 273);
+            this.numeric1.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numeric1.Name = "numeric1";
+            this.numeric1.Size = new System.Drawing.Size(140, 20);
+            this.numeric1.TabIndex = 17;
+            this.numeric1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numeric1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 40);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Print Unsorted Array";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 414);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numeric1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.SayiUret);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numeric1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button SayiUret;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown numeric1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        
+
+        Double[] sayilar;
+        Double[] Dizi;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        
+        //dizimizi global tanımlıyoruz
+        /**---------------------------MERGE-------------------------------**/
+        static void MergeYap(Double[] numbers, int left, int mid, int right)
+        {
+
+            Double[] temp = new Double[numbers.Length];
+            
+            
+            int i, left_end, num_elements, tmp_pos;//merge sort iki fonx.'dan oluşuyor. RecursiveMerge bu 
+                                                    //bu fonx çağıracak
+
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                  temp[tmp_pos++] = numbers[left++];
+                else
+                    temp[tmp_pos++] = numbers[mid++];
+            }
+
+            while (left <= left_end)
+                temp[tmp_pos++] = numbers[left++];
+
+            while (mid <= right)
+                temp[tmp_pos++] = numbers[mid++];
+
+            for (i = 0; i < num_elements; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        
+        }
+
+        static void MergeSort_Recursive(Double[] numbers, int left, int right)
+        {
+            int mid;
+
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                MergeSort_Recursive(numbers, left, mid);
+                MergeSort_Recursive(numbers, (mid + 1), right);
+
+                MergeYap(numbers, left, (mid + 1), right);
+            }
+        }
+        /**--------------------------MERGE----------------------------**/
+
+
+
+        /**---------------------Quick------------------------------**/
+        static void QuickSort(Double[] x, int first, int last)
+        {
+            int pivot; int j; Double temp; 
+                int i;
+            
+            if (first < last)
+            {
+                pivot = first;
+                i = first;
+                j = last;
+
+                while (i < j)
+                {
+                    while (x[i] <= x[pivot] && i < last)
+                        i++;
+                    while (x[j] > x[pivot])
+                        j--;
+                    if (i < j)
+                    {
+                        temp = x[i];
+                        x[i] = x[j];
+                        x[j] = temp;
+                    }
+                }
+
+                temp = x[pivot];
+                x[pivot] = x[j];
+                x[j] = temp;
+                QuickSort(x, first, j - 1);
+                QuickSort(x, j + 1, last);
+
+            }
+        }
+        /**-------------------------Quick----------------------**/
+        
+        private void label1_Click(object sender, EventArgs e)//siralama zamani
+        {
+
+        }
+        void degerata()
+        {
+            sayilar = new double[Dizi.Length];
+            for (int i = 0; i < Dizi.Length; i++)
+            {
+                sayilar[i] = Dizi[i];
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)//sayi üret
+        {
+            
+            Dizi=new double[(int)numeric1.Value];
+            
+            Stopwatch sw = new Stopwatch();
+            
+            int i, k;
+            Random rnd = new Random();
+            sw.Start();
+            listBox1.Items.Clear();
+            for (i = 0; i < Dizi.Length; i++)
+            {
+                k = rnd.Next(1, 100);
+                Dizi[i] = k;
+            }
+            sw.Stop();
+            string runningTime = sw.Elapsed.ToString();
+            label3.Text = runningTime;
+
+           
+            
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)//selection sort
+        {
+            int i;
+            int j;
+            Double tasinan;
+            //Double[] sayilar = new double[(int)numeric1.Value];
+            degerata();
+            Stopwatch sw = new Stopwatch();
+
+            listBox2.Items.Clear();
+            sw.Start();
+            for (i = 1; i < sayilar.Length; i++)
+            {
+
+                tasinan = sayilar[i];
+                j = i;
+                while (j > 0 && sayilar[j - 1] > tasinan)
+                {
+                    sayilar[j] = sayilar[j - 1];
+                    j--;
+                }
+                sayilar[j] = tasinan;
+            }
+            sw.Stop();
+            string runningTime = sw.Elapsed.ToString();
+            label4.Text = runningTime;
+        }
+            
+        private void button3_Click(object sender, EventArgs e)//quick sort click
+                    {
+                        Stopwatch sw = new Stopwatch();
+                        listBox2.Items.Clear();
+                        degerata();
+                        sw.Start();
+                        QuickSort(sayilar, 0, sayilar.Length - 1);
+                        sw.Stop();
+                        string runningTime = sw.Elapsed.ToString();
+                        label6.Text = runningTime;
+                        
+                    }
+
+        
+
+        private void button4_Click(object sender, EventArgs e)//merge sort Click
+        {
+            Stopwatch sw = new Stopwatch();
+            listBox2.Items.Clear();
+            degerata();
+            sw.Start();
+            MergeSort_Recursive(sayilar, 0, sayilar.Length-1);
+            sw.Stop();
+            string runningTime = sw.Elapsed.ToString();
+            label8.Text = runningTime;
+        }
+
+        private void button5_Click(object sender, EventArgs e)//yazdir_click
+        {
+            int i;
+            for (i = 0; i < sayilar.Length; i++)
+            {
+                listBox2.Items.Add(sayilar[i].ToString());
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)//random üretme zamani
+        {
+
+        }
+
+       
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)//random üretilen sayilar yazilir
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)//siralanmis sayilar yazilir
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)// random Sayilari listbox 1 e yazdir
+        {
+            int i;
+            for (i = 0; i < Dizi.Length; i++)
+            {
+                listBox1.Items.Add(Dizi[i].ToString());
+            }
+        }
+    }
+}
